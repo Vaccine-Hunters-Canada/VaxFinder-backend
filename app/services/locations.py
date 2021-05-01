61b8356baf75583ae627079ea97dba79dd19da52
Alvin Tang <alvin.tang@mail.utoronto.ca>
2021-05-01 16:21:09 -0400

    :pencil2: Fixed mypy errors.

--- app/services/locations.py
+++ app/services/locations.py
@@ -1,8 +1,9 @@
-from app.schemas.misc import FilterParamsBase
 from typing import List, Optional, Type
+
 from loguru import logger
 
 from app.schemas.locations import LocationExpandedResponse, LocationResponse
+from app.schemas.misc import FilterParamsBase
 from app.services.addresses import AddressService
 from app.services.base import BaseService
 
@@ -14,49 +15,47 @@ class LocationService(
 
     @property
     def table(self) -> str:
-        return 'locations'
+        return "locations"
 
     @property
     def db_response_schema(self) -> Type[LocationResponse]:
         return LocationResponse
 
-    async def get_by_id_expanded(self, id: int) -> Optional[LocationExpandedResponse]:
+    async def get_by_id_expanded(
+        self, id: int
+    ) -> Optional[LocationExpandedResponse]:
         location = await super().get_by_id(id)
 
         if location is not None:
-            address = await AddressService(self._db).get_by_id(
-                location.address
-            )
-
-            assert (
-                address is not None
-            ), f'Could not find address {location.address} for entry {location.id}'
+            address = None
+            if location.address is not None:
+                address = await AddressService(self._db).get_by_id(
+                    location.address
+                )
 
             location_expanded = LocationExpandedResponse(
-                **location.dict(),
-                address=address
+                **location.dict(), address=address
             )
             return location_expanded
 
         return location
 
-    async def get_all_expanded(self, filters: Optional[FilterParamsBase] = None) -> List[LocationExpandedResponse]:
+    async def get_all_expanded(
+        self, filters: Optional[FilterParamsBase] = None
+    ) -> List[LocationExpandedResponse]:
         locations = await super().get_all(filters)
 
         # TODO: should be done all at once instead of in a for loop
         locations_expanded: List[LocationExpandedResponse] = []
         for location in locations:
-            address = await AddressService(self._db).get_by_id(
-                location.address
-            )
+            address = None
+            if location.address is not None:
+                address = await AddressService(self._db).get_by_id(
+                    location.address
+                )
 
-            assert (
-                address is not None
-            ), f'Could not find address {location.address} for entry {location.id}'
-            
-            locations_expanded.append(LocationExpandedResponse(
-                **location.dict(),
-                address=address
-            ))
+            locations_expanded.append(
+                LocationExpandedResponse(**location.dict(), address=address)
+            )
 
         return locations_expanded
--- app/services/organizations.py
+++ app/services/organizations.py
@@ -1,11 +1,12 @@
-from typing import Type
+from typing import Optional, Type
+
+from loguru import logger
 
 from app.schemas.organizations import (
     OrganizationCreateRequest,
     OrganizationResponse,
 )
 from app.services.base import BaseService
-from loguru import logger
 
 
 class OrganizationService(
@@ -14,17 +15,20 @@ class OrganizationService(
     ]
 ):
     read_procedure_id_parameter = "organizationID"
-    
+
     @property
     def table(self) -> str:
-        return 'organizations'
+        return "organizations"
 
     @property
     def db_response_schema(self) -> Type[OrganizationResponse]:
         return OrganizationResponse
 
     async def create(
-        self, full_name: str, short_name: str, description: str
+        self,
+        full_name: Optional[str],
+        short_name: str,
+        description: Optional[str],
     ) -> None:
         await self._db.execute_stored_procedure(
             query="""
